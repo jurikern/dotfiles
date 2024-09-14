f2d96c9c13316922b805f47f5823abe1528e3cd3
Juri Kern <jkern@malwarebytes.com>
2024-09-14 10:07:51 -0700

    check battery

---
+++ commands/bat0.sh
@@ -0,0 +1,3 @@
+chmod +x bat0.sh
+systemctl --user start bat0.service
+systemctl --user enable bat0.service
--- dunst/.config/dunst/bat0.sh
+++ dunst/.config/dunst/bat0.sh
@@ -4,8 +4,10 @@ CAPACITY=$(cat /sys/class/power_supply/BAT1/capacity)
 STATUS=$(cat /sys/class/power_supply/BAT1/status)
 
 [ "$STATUS" = "Discharging" ] && [ "$CAPACITY" -lt 21 ] && \
-/usr/bin/notify-send -u critical -a power_supply_low "Low Battery ($CAPACITY%)" "Plug in the charger."
+/usr/bin/notify-send -u critical -a power_supply_low "Low Battery ($CAPACITY%)" "Plug in the charger." || \
+echo "Full Battery notification not sent"
 
-[ "$STATUS" = "Charging" ] && [ "$CAPACITY" -gt 80 ] && \
-/usr/bin/notify-send -u normal -a power_supply_full "Battery Full ($CAPACITY%)" "Unplug from charger."
+[ "$STATUS" = "Charging" ] && [ "$CAPACITY" -gt 79 ] && \
+/usr/bin/notify-send -u normal -a power_supply_full "Battery Full ($CAPACITY%)" "Unplug from charger." || \
+echo "Full Battery notification not sent"
 
