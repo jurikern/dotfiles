2692af4483f8fb85e5547868b328d4894948e024
Juri Kern <juri.kern@illumio.com>
2025-01-02 09:19:12 -0800

    update wezterm config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -3,10 +3,10 @@ local config = wezterm.config_builder()
 local act = wezterm.action
 
 config.font = wezterm.font("MesloLGS Nerd Font Mono")
-config.font_size = 12
+config.font_size = 16
 
 config.enable_tab_bar = false
-config.window_decorations = "NONE"
+config.window_decorations = "RESIZE"
 
 config.color_scheme = "Catppuccin Mocha"
 config.window_background_opacity = 0.9
