adac0d1fbf7ee421e025a1af55b0de14e54f98b0
Juri Kern <juri@secbase.us>
2025-02-17 22:04:57 -0800

    update theme

--- lazyvim/.config/nvim/lua/jkern/plugins/telescope.lua
+++ lazyvim/.config/nvim/lua/jkern/plugins/telescope.lua
@@ -43,7 +43,7 @@ return {
         },
         file_browser = {
           theme = "ivy",
-          hijack_netrw = true,
+          hijack_netrw = false,
 
           mappings = {
             ["i"] = {
@@ -63,7 +63,8 @@ return {
     local keymap = vim.keymap
 
     keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>")
-    keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>")
+    keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles theme=ivy<cr>")
+    keymap.set("n", "<leader>fR", "<cmd>Telescope frecency theme=ivy<cr>")
     keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>")
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>")
     keymap.set("n", "<leader>fd", "<cmd>Telescope file_browser<cr>")
